56a4322b32e1be417a328d48e10499b8f4ae47e3
José Lídio de Oliveira Júnior <jose.junior@oletecnologia.com.br>
2021-02-02 16:19:53 -0300

    Adicionado projeto base

---
+++ Source/ProcessoAvaliacao.WebApi/Dtos/FilmesGet.cs
@@ -0,0 +1,17 @@
+﻿using Otc.ComponentModel.DataAnnotations;
+
+namespace ProcessoAvaliacao.WebApi.Dtos
+{
+    public class FilmesGet
+    {
+        /// <summary>
+        /// Termo a ser pesquisado.
+        /// </summary>
+        public string TermoPesquisa { get; set; }
+
+        /// <summary>
+        /// Ano de lançamento.
+        /// </summary>
+        public int? AnoLancamento { get; set; }
+    }
+}
---
+++ Source/ProcessoAvaliacao.WebApi/WebApiMapperProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using ProcessoAvaliacao.Domain.Models;
+using ProcessoAvaliacao.WebApi.Dtos;
+
+namespace ProcessoAvaliacao.WebApi
+{
+    public class WebApiMapperProfile : Profile
+    {
+        public WebApiMapperProfile()
+        {
+            CreateMap<Filme, FilmesGetResult>();
+            CreateMap<FilmesGet, Pesquisa>();
+        }
+    }
+}
